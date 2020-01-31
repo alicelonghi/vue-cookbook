accfb2efb26ba9381598bc5bc8062478393243ba
alicelonghi <alicellonghi@gmail.com>
2020-01-30 22:46:12 -0300

    add project with more than one component

---
+++ 04-componentes-juntos/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  	<!-- faz uso das tags dos componentes registrados -->
+    <lvBusca></lvBusca>
+    <hr>
+    <lv-perfil></lv-perfil>
+  </div>
+</template>
+
+<script>
+// importa arquivo dos componentes
+import lvBusca from './LvBusca.vue'
+import lvPerfil from './LvPerfil.vue'
+
+export default {
+  name: 'app',
+  // declara componentes para usar aqui
+  components: {lvBusca, lvPerfil}
+}
+</script>
