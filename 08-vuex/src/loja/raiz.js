7a6df23c753381e99e51c3012a1a4d061a9a0c7d
alicelonghi <alicellonghi@gmail.com>
2020-01-30 23:58:22 -0300

    add vuex example

---
+++ 08-vuex/src/loja/raiz.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+import estado from './estado'
+import Tarefas from './modulos/tarefas/raiz'
+
+export default new Vuex.Store({
+	state: estado,
+	modules: {
+		Tarefas
+	}
+
+})
---
+++ 08-vuex/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import loja from './loja/raiz'
+
+new Vue({
+  store: loja,
+  el: '#app',
+  render: h => h(App)
+})
